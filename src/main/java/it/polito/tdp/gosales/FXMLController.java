88b0b6aa5a43822be709c99f1d0d14238ccd93f8
JosephAv <g.averta3@gmail.com>
2023-07-14 17:10:27 +0200

    sol v1

--- src/main/java/it/polito/tdp/gosales/FXMLController.java
+++ src/main/java/it/polito/tdp/gosales/FXMLController.java
@@ -1,17 +1,20 @@
 package it.polito.tdp.gosales;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
-import it.polito.tdp.gosales.model.Model;
+import it.polito.tdp.gosales.model.Arco;
+import it.polito.tdp.gosales.model.Products;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
+import it.polito.tdp.gosales.model.Model;
+
 
 public class FXMLController {
-
     private Model model;
 
     @FXML
@@ -27,13 +30,13 @@ public class FXMLController {
     private Button btnCreaGrafo;
 
     @FXML
-    private ComboBox<?> cmbAnno;
+    private ComboBox<Integer> cmbAnno;
 
     @FXML
-    private ComboBox<?> cmbColore;
+    private ComboBox<String> cmbBrand;
 
     @FXML
-    private ComboBox<?> cmbProdotto;
+    private ComboBox<Products> cmbProdotto;
 
     @FXML
     private TextArea txtArchi;
@@ -45,7 +48,33 @@ public class FXMLController {
     private TextArea txtResult;
 
     @FXML
+    void doCercaPercorso(ActionEvent event) {
+
+    }
+
+    @FXML
     void doCreaGrafo(ActionEvent event) {
+
+        String brand = cmbBrand.getValue();
+        Integer year = cmbAnno.getValue();
+
+       this.model.buildGraph(brand, year);
+
+        txtResGrafo.setText("Num Nodi: " + this.model.getNumNodi()+"\n");
+        txtResGrafo.appendText("Num Archi: " + this.model.getNumArchi());
+
+        txtArchi.setText("Top 3 archi: \n");
+        List<Arco> topArchi = this.model.getTopArchi();
+
+        for (Arco a : topArchi)
+            txtArchi.appendText(a+"\n");
+
+        txtArchi.appendText("Nodi che compaiono almeno due volte:\n");
+        List<Integer> repeatedProdsID = this.model.repeatedProducts();
+
+        for (Integer i : repeatedProdsID)
+            txtArchi.appendText("Product ID : " + i + "\n");
+
 
     }
 
@@ -54,16 +83,20 @@ public class FXMLController {
         assert btnCerca != null : "fx:id=\"btnCerca\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnCreaGrafo != null : "fx:id=\"btnCreaGrafo\" was not injected: check your FXML file 'Scene.fxml'.";
         assert cmbAnno != null : "fx:id=\"cmbAnno\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert cmbColore != null : "fx:id=\"cmbColore\" was not injected: check your FXML file 'Scene.fxml'.";
         assert cmbProdotto != null : "fx:id=\"cmbProdotto\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert cmbBrand != null : "fx:id=\"cmbTipo\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtArchi != null : "fx:id=\"txtArchi\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResGrafo != null : "fx:id=\"txtResGrafo\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 
-    public void setModel(Model model) {
-    	this.model = model;
+    public void setModel(Model model){
+        this.model=model;
+        cmbAnno.getItems().setAll(this.model.getAllYears());
+        cmbBrand.getItems().setAll(this.model.getAllBrands());
+
     }
+
 
 }
--- src/main/java/it/polito/tdp/gosales/dao/testDAO.java
+++ src/main/java/it/polito/tdp/gosales/dao/testDAO.java
@@ -3,6 +3,7 @@ package it.polito.tdp.gosales.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import it.polito.tdp.gosales.model.Arco;
 import it.polito.tdp.gosales.model.DailySale;
 import it.polito.tdp.gosales.model.Products;
 import it.polito.tdp.gosales.model.Retailers;
@@ -10,9 +11,23 @@ import it.polito.tdp.gosales.model.Retailers;
 public class testDAO {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		GOsalesDAO dao = new GOsalesDAO();
-		List<Retailers> listR = new ArrayList<>();
+		List<Products> prod = new ArrayList<>();
+
+		prod = dao.getNodes("Relax");
+
+		for (Products p : prod)
+			System.out.println(p);
+
+		System.out.println(prod.size()+"");
+
+
+
+
+		// TODO Auto-generated method stub
+
+	/*	List<Retailers> listR = new ArrayList<>();
 		List<Products> listP = new ArrayList<>();
 		List<DailySale> listS = new ArrayList<>();
 		
@@ -27,7 +42,7 @@ public class testDAO {
 		System.out.println(listS.size());
 		
 		
-		
+		*/
 	}
 
 }
